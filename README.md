f611c7012a278d1da65ae29b9bc6af01614cf245
Caio Felipe de Araujo <c.felipe.araujo@gmail.com>
2021-01-02 14:10:35 -0300

    criacao da app produto

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ Para desativar basta digitar `deactivate`.
 Instalar as dependências:<br>
 Nota: Certifique-se de que a venv esteja ativada.<br>
 `pip install -r requirements.txt`
+
+### Criação do banco de dados
+Execute: `python migrate.py migrate`
+
+O banco de dados é salvo local no arquivo `db.sqlite3`.
+
+### Criação do usuário admin
+Execute: `python -m createsuperuser`
+
+Preencha as informações solicitadas e utilize as credenciais no módulo admin (/admin).
+
+## Run
+Na raíz do projeto: `python manage.py runserver`
---
+++ produto/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Produto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=250, help_text="Nome do produto")
+    marca = models.CharField(max_length=250, help_text="Nome da fabricante")
+    quantidade_disponivel = models.IntegerField(help_text="Quantidade em estoque")
+    valor = models.DecimalField(max_digits=20, decimal_places=2, help_text="Valor em BRL")
+    data_criado = models.DateTimeField(auto_now_add=True)
+    data_alterado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'produto'
+        ordering = ['-id']
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
