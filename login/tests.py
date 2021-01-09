6b2ac704f774f3c81b1191ee730bee6c51e8b4b8
Caio Felipe de Araujo <c.felipe.araujo@gmail.com>
2021-01-09 19:27:19 -0300

    add tests for logout

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,18 @@
+from django.contrib import auth
+from django.contrib.auth.models import User
 from django.test import TestCase
+from model_mommy import mommy
 
-# Create your tests here.
+
+class TestLogout(TestCase):
+    def setUp(self):
+        self.user = mommy.make(User, username='Usuario1')
+
+    def test_logout(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/logout', follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, '/')
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
