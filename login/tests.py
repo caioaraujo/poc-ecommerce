214d9811dba0389665d503468a7306ec9ccbc660
Caio Felipe de Araujo <c.felipe.araujo@gmail.com>
2021-04-03 21:05:02 -0300

    fix TestLogin

--- login/tests.py
+++ login/tests.py
@@ -1,4 +1,5 @@
 from django.contrib import auth
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase
 from model_mommy import mommy
@@ -20,12 +21,11 @@ class TestLogout(TestCase):
 
 class TestLogin(TestCase):
     def setUp(self):
-        self.user = mommy.make(User, username='Usuario1', password='123abc')
+        self.user = get_user_model().objects.create_user(username='Usuario1', password='123abc')
 
     def test_login__success(self):
         response = self.client.post('/', {"usuario": "Usuario1", "senha": "123abc"})
 
-        self.assertEqual(response.status_code, 200)
-        #self.assertRedirects(response, '/produtos/')
-        # user = auth.get_user(self.client)
-        # self.assertTrue(user.is_authenticated)
+        self.assertRedirects(response, '/produtos/', status_code=302)
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated)
